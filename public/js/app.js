0501858ca95ba3bde26875bc65e9c10c961b6974
CoderYooda <coderyooda@gmail.com>
2020-10-28 17:56:26 +0300

    awd

--- public/js/app.js
+++ public/js/app.js
@@ -55373,8 +55373,8 @@ vue__WEBPACK_IMPORTED_MODULE_0___default.a.use(vuex__WEBPACK_IMPORTED_MODULE_1__
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\OSPanel\domains\manager\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\OSPanel\domains\manager\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\OpenServer\domains\ManagerCRM\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\OpenServer\domains\ManagerCRM\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })

72a081f9303322a8c0a6147e117a82368f73cefa
CoderYooda <coderyooda@gmail.com>
2020-11-13 17:14:55 +0300

    Scriptor

--- resources/js/Classes/Canva.js
+++ resources/js/Classes/Canva.js
@@ -39,25 +39,26 @@ class Path{
         this.tag.setAttribute('stroke-width', '2');
         this.tag.setAttribute('fill', 'transparent');
     }
-
     setId(id){
         this.tag.setAttribute('id', id);
     }
-
     calculateScalar(){
         this.orientation_scalar = Math.sqrt(Math.pow( (this.end[0] - this.start[0]) , 2) + Math.pow( (this.end[1] - this.start[1]) , 2)) / 2;
     }
-
-
-    fresh(start, end){
+    freshStart(start){
         this.start = start;
+        let startOrientation = this.getStartOrientation();
+        let endOrientation = this.getEndOrientation();
+        this.calculateScalar();
+        this.tag.setAttribute('d', 'M' + this.start[0] + ' ' + this.start[1] + ' C ' + startOrientation[0] + ' ' + startOrientation[1] + ', ' + endOrientation[0] + ' ' + endOrientation[1] + ', ' + this.end[0] + ' ' + this.end[1]);
+    }
+    freshEnd(end){
         this.end = end;
         let startOrientation = this.getStartOrientation();
         let endOrientation = this.getEndOrientation();
         this.calculateScalar();
         this.tag.setAttribute('d', 'M' + this.start[0] + ' ' + this.start[1] + ' C ' + startOrientation[0] + ' ' + startOrientation[1] + ', ' + endOrientation[0] + ' ' + endOrientation[1] + ', ' + this.end[0] + ' ' + this.end[1]);
     }
-
     getStartOrientation(){
         let orientation = [0,0];
         orientation[0] = Math.sin(this.start[2] * PI / 180) * this.orientation_scalar + this.start[0];
@@ -73,7 +74,55 @@ class Path{
         return orientation;
     }
 }
+
+class TextBlock{
+    constructor(options){
+        this.position = options.position;
+        this.text = options.text;
+        this.canva = options.canva;
+        this.attached = options.attached;
+        this.dragged = false;
+        this.width = 100;
+        this.height = 100;
+        this.touch_shift = [0,0];
+        this.init();
+    }
+    init(){
+        this.tag = document.createElementNS("http://www.w3.org/2000/svg", 'rect');
+        this.tag.setAttribute('x', this.position[0]);
+        this.tag.setAttribute('y', this.position[1]);
+        this.tag.setAttribute('stroke', 'rgb(111 212 0)');
+        this.tag.setAttribute('width', '100');
+        this.tag.setAttribute('height', '100');
+
+        document.addEventListener('mouseup', (e)=>{
+            this.dragged = false;
+        });
+
+        this.canva.canv.addEventListener('mousemove', (e)=>{
+            if(this.dragged){
+                let x = e.offsetX;
+                let y = e.offsetY;
+                this.tag.setAttribute('x', x - this.touch_shift[0]);
+                this.tag.setAttribute('y', y - this.touch_shift[1]);
+
+                this.position = [x,y];
+            }
+        });
+        this.tag.addEventListener('mousedown', (e)=>{
+            this.touch_shift = [e.offsetX - this.tag.x.animVal.value, e.offsetY - this.tag.y.animVal.value];
+            this.dragged = true;
+        });
+    }
+    attachPath(options){
+        this.attached.push(options.path);
+    }
+    setId(id){
+        this.tag.setAttribute('id', id);
+    }
+}
 module.exports = {
-    Canva : Canva,
-    Path : Path
+    Canva: Canva,
+    Path: Path,
+    TextBlock: TextBlock
 };
--- resources/js/components/views/Scripture.vue
+++ resources/js/components/views/Scripture.vue
@@ -7,7 +7,7 @@
 </template>
 
 <script>
-    import {Canva, Path} from "../../Classes/Canva"
+    import {Canva, Path, TextBlock} from "../../Classes/Canva"
     export default {
         created(){
 
@@ -23,13 +23,24 @@
                 start: [300,300, 90],
                 end: [300,500, 90],
             });
+
+            let block = new TextBlock({
+                position: [300,300],
+                attached:{
+                    path:path,
+                    connection: 'Start'
+                },
+                canva: canva,
+                text: 'Тестовый блок',
+            });
+
+
             canva.addElem(path);
-
-
-
+            canva.addElem(block);
 
             document.getElementsByClassName('canva')[0].addEventListener('mousemove', (e) => {
-                path.fresh([300,300, 90], [e.offsetX, e.offsetY, 90]);
+                path.freshStart([300,300, 90]);
+                path.freshEnd([e.offsetX, e.offsetY, 90]);
             })
         },
         methods: {
